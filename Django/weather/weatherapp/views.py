d21a4a3d69243832bd12258e57c91a15b2efd37e
Alok <waynerooney0089@gmail.com>
2024-03-25 00:36:20 +0530

    Cored Deleted

--- Django/weather/weatherapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-import json
-import requests
-# Create your views here.
-def weatherapp(request):
-    if request.method=="POST":
-        city=request.POST["city"]
-        source=""
-    return HttpResponse("<h1>Alok</h1>")
